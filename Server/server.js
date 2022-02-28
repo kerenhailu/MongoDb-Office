3fcd70b929f226a9108f2c7555ef0ced3e433a9a
kerenhailu <keren4509@gmail.com>
2022-02-28 17:02:25 +0200

    delete

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ const controllers=require('./Controllers/EmployeeController')
 const RouterEmployee = require('./Routs/EmployeeRout');
 const app=express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.listen(process.env.PORT)
+app.get('/',(req,res)=>res.send("is working"))
 app.use('/employees',RouterEmployee)
-app.get('/',(req,res)=>res.send("is working "))
