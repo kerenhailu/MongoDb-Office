c0db692453f5650a3a8bdab1f53892abbe20b02b
kerenhailu <keren4509@gmail.com>
2022-03-01 14:23:40 +0200

    register

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -2,12 +2,13 @@ const Users = require("../Models/EmployeeModel");
 const bcrypt = require("bcrypt");
 const register = async (req, res) => {
   if (Users.exists(req.body.email)==true)
-    return res.status(400).send({ massage: "email already exists" });
+    return res.status(400).json({ massage: "email already exists" });
   bcrypt.hash(req.body.password, 10,async (err, hashPass) => {
-    if (err) return res.status(500).send({ massage: "error in password hash" });
-    await Users.create({ ...req.body, password: hashPass })
-      .then((result) => res.status(200).send({ massage: "succsfuly" }, result))
-      .catch((err) => res.status(404).send({ massage: err }));
+    if (err) return res.status(500).json({ massage: "error in password hash" });
+    req.body.password=hashPass;
+    await Users.create(req.body)
+      .then((result) => res.status(200).json({ massage: "succsfuly", result }))
+      .catch((err) => res.status(404).json({ massage: err }));
   });
 };
-module.exports =register
+module.exports ={register}
--- Server/Routs/UserRout.js
+++ Server/Routs/UserRout.js
@@ -2,6 +2,6 @@ const RouterUser=require('express').Router();
 const register = require('../Controllers/UserController');
 
 
-RouterUser.post('/register',register)
+RouterUser.post('/',register.register)
 
 module.exports =RouterUser
