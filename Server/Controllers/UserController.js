47fe1592a5c52a5fbb48d73a9beeeab1e9b7a923
kerenhailu <keren4509@gmail.com>
2022-03-01 15:50:00 +0200

    jwt

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -1,5 +1,6 @@
 const Users = require("../Models/EmployeeModel");
 const bcrypt = require("bcryptjs");
+const jwt=require('jsonwebtoken')
 const register = async (req, res) => {
   if (Users.exists(req.body.email)==true)
     return res.status(400).json({ massage: "email already exists" });
@@ -11,4 +12,16 @@ const register = async (req, res) => {
       .catch((err) => res.status(500).json({ massage: err }));
   });
 };
-module.exports ={register}
+
+const logIn=async(req,res)=>{
+  if(Users.exists(req.body.email)==true) return res.status(400).json({ massage: "email already exists" });
+  const {email,password}=req.body
+  const user = Users.findOne(user=>user.email===email);
+  bcrypt.compare(password,user.password,(err,isMatch)=>{
+    if(err)return res.status(500).json({ massage: "err in compare" });
+    if(!isMatch)  return res.status(403).json({ massage: "incorrect password" })
+    jwt.sign(user,process.env.SECRET_OR_KEY),{expiresIn:3600},(err,token)=>{}
+    res.status(200).json({massage:"logIn successful",user})
+  })
+}
+module.exports ={register,logIn}
