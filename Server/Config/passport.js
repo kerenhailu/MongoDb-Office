83f6ef46e019c8e28fe3f5a0d2098ac512163b69
kerenhailu <keren4509@gmail.com>
2022-03-02 11:38:35 +0200

    jwt

--- Server/Config/passport.js
+++ Server/Config/passport.js
@@ -2,26 +2,34 @@
 const StrategyJWT = require("passport-jwt").Strategy;
 // ועל מנת לחלץ מידע נייבא אותו
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-// לייבא מודל של יוזר
+//  כדי לעבוד מול הקולקשן ייבא מודל של יוזר
 const Users = require("../Models/UserModel");
 // את האיך יש לנו למעלה ייבואנו אבל באיזה שפה אנחנו לא יודעים
 // ובגלל זה נבייא גם את הסיקרט שלנו
 const SECERET_KEY = process.env.SECERET_KEY;
-const options = {};
+const options = {
+secretOrKey:process.env.SECERET_KEY
+
+};
 // מחזיק את התוקן מהבקשה
 // הפונקציה הולכת לריקווסט ומוציאה משם את התוקן
+// הןא מחוץ לאובייקט כדי שנפעיל את הפונקציה ויביא לי רק את התוקן 
+// ולא יחשוב שמביאים לו את הפונקציה עצמה
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 
 // הפןנקציה מתווכת
 const passportFunction = (passport) => {
   passport.use(
-    new StrategyJWT(options, (jwt_payload, done) => {
-      Users.findOne({ _id: jwt_payload.id })
+    new StrategyJWT(options, (obj_user_from_payload, done) => {
+      Users.findOne({ _id: obj_user_from_payload._id })
         .then((user) => {
-          if (user) return done(null, user);
+            // פרמטר ראשון של פונקצית דן היא פרמטר שגיאה , פרמטר שני הצלחה/אובייקט שנרצה לעבוד איתו(מידע)
+          if (user) done(null, user);
           done(null, false);
         })
         .catch((err) => done(err, false));
     })
   );
 };
+
+module.exports={passportFunction};
