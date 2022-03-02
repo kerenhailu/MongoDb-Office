5f4f0cdd651f76ab5cda6d211ac5b50d5bc92d99
kerenhailu <keren4509@gmail.com>
2022-03-02 12:26:54 +0200

    s

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
-const express=require('express');
-require('./DB');
-const cors=require('cors');
-const RouterEmployee = require('./Routs/EmployeeRout');
-const RouterUser=require('./Routs/UserRout')
+require("dotenv").config();
+const express = require("express");
+require("./DB");
+const cors = require("cors");
+const RouterEmployee = require("./Routs/EmployeeRout");
+const RouterUser = require("./Routs/UserRout");
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(cors());
-const PORT =process.env.PORT ||1998 
-app.listen(PORT)
+const PORT = process.env.PORT || 1998;
+app.listen(PORT);
 // app.register('/',(req,res)=>res.send('register'))
 // app.get('/',(req,res)=>res.send('workk'))
 // app.post('/',(req,res)=>res.send('add'))
@@ -18,5 +18,5 @@ app.listen(PORT)
 // app.use('/employees',RouterEmployee)
 
 // http://localhost:1998/auth/register
-app.get('/',(req,res)=>res.send("gfgf"))
-app.use('/auth',RouterUser)
+app.get("/", (req, res) => res.send("gfgf"));
+app.use("/auth", RouterUser);
