ac9c531ef789c791f79433b4e9ceeb78a3d373c3
kerenhailu <keren4509@gmail.com>
2022-03-02 08:41:23 +0200

    server

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -16,12 +16,17 @@ const register = async (req, res) => {
 const logIn=async(req,res)=>{
   if(Users.exists(req.body.email)==true) return res.status(400).json({ massage: "email already exists" });
   const {email,password}=req.body
-  const user = Users.findOne(user=>user.email===email);
+  const user =await Users.findOne(user=>user.email===req.body.email)
+  .then(user=>{
+
+  })
   bcrypt.compare(password,user.password,(err,isMatch)=>{
     if(err)return res.status(500).json({ massage: "err in compare" });
     if(!isMatch)  return res.status(403).json({ massage: "incorrect password" })
-    jwt.sign(user,process.env.SECRET_OR_KEY),{expiresIn:3600},(err,token)=>{}
-    res.status(200).json({massage:"logIn successful",user})
+    jwt.sign(user,process.env.SECRET_KEY),{expiresIn:'30m'},(err,token)=>{
+      if(err)return res.status(500).json({ massage: "err in token" });
+      res.status(200).json({massage:"logIn successful",token})
+    }
   })
 }
 module.exports ={register,logIn}
--- Server/Routs/UserRout.js
+++ Server/Routs/UserRout.js
@@ -3,5 +3,6 @@ const register = require('../Controllers/UserController');
 
 
 RouterUser.post('/',register.register)
+RouterUser.post('/login',register.logIn)
 
 module.exports =RouterUser
